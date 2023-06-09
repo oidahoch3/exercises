ad6c694fe0f7f50b81062e2914d99b67bed3d6fa
oidahoch3 <walch.anna@hotmail.com>
2023-06-09 15:59:03 +0200

    change of temperature threshold logic

--- temperature.py
+++ temperature.py
@@ -34,28 +34,37 @@
 #
 # Note: The numbers entered should be treated as floating-point values for accurate calculations.
 
-temperature = []  # creating a list
+temperatures = []  # creating a list
 
 while True:
     add = input('Enter a temperature: ')  # asking for input -> entering temperatures
 
     try:
-        temperature.append(float(add))  # added temps are added to the list and the string is converted to float
+        temperatures.append(float(add))  # added temps are added to the list and the string is converted to float
     except ValueError:  # if the input is no number, the loop stops
         break
 
-threshold = float(input('Enter a threshold for the temperature: '))  # asking for another input to determine a threshold
+threshold = None  # creating threshold variable
 
-above_threshold = [temp for temp in temperature if
-                   temp > threshold]  # creating a list where all the temps above the threshold are added
-count_above_threshold = len(above_threshold)  # creating a variable that tells me the length of the list i just created
+while threshold is None:  # starting loop to ask for valid threshold input
+    try:
+        threshold = float(input('Enter a threshold for the temperatures: '))  # asking for threshold input
+    except ValueError:  # if its not valid input, it starts the loop again
+        pass
 
-sumOfTemp = sum(temperature)  # getting the sum of all the entered temps
-countTemp = len(temperature)  # getting the amount of how many values are in the list
+above_threshold = []  # creating a list for temps that are above the threshold
 
-aveTemp = sumOfTemp / countTemp  # calculating average temperature
+for temp in temperatures:  # checking if temps in temperatures list are above threshold
+    if temp > threshold:
+        above_threshold.append(temp)  # if they are, they get added to the list
 
-print('The average Temperature is: ', aveTemp)
-print('The highest Temperature is: ', max(temperature))  # finding the max value in the temp list
-print('The lowest Temperature is: ', min(temperature))  # finding the min value in the temp list
-print('Number of temperatures above threshold', threshold, ':', count_above_threshold)
+sum_of_temp = sum(temperatures)  # getting the sum of all the entered temps
+count_of_temp = len(temperatures)  # getting the amount of how many values are in the list
+
+avg_temp = sum_of_temp / count_of_temp  # calculating average temperature
+
+print('The average Temperature is:', avg_temp)
+print('The highest Temperature is:', max(temperatures))  # finding the max value in the temp list
+print('The lowest Temperature is:', min(temperatures))  # finding the min value in the temp list
+print('Number of temperatures above', str(threshold) + ':',
+      len(above_threshold))  # printing the length of my above_threshold list
